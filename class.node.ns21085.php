1a6980ca7b01eab7b3445cf5dc58fbe27d10782c
Andrej A Antonov <polymorphm@gmail.com>
2010-07-08 17:34:02 +0400

    информация о лицензии (GNU AGPL)

--- class.node.ns21085.php
+++ class.node.ns21085.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+    This file is part of List58.
+
+    List58 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    List58 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with List58.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 require_once dirname(__FILE__).'/class.node_base.ns8054.php';
 
 class node__ns21085 extends node_base__ns8054 {
