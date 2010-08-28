b1421a656ac498b1064ac6162e724cf9abe76f98
Andrej A Antonov <polymorphm@gmail.com>
2010-08-28 05:37:22 +0400

    главная страница помечена как требующая резрешения на поиск

--- class.node_base.ns8054.php
+++ class.node_base.ns8054.php
@@ -94,6 +94,13 @@ class node_base__ns8054 {
         mysql_query('COMMIT', $this->_node_base__db_link);
     }
     
+    protected function _node_base__add_check_perms($perms) {
+        $this->_node_base__need_check_perms = array_merge(
+            $this->_node_base__need_check_perms,
+            $perms
+        );
+    }
+    
     protected function _node_base__check_auth() {
         // TODO: эта функция долна быть расширена для более глубокой проверки!
         
