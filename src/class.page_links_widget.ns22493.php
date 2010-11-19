90ff1c6e24cbe9ce5afa7fa2cf10787d9523c903
Andrej A Antonov <polymorphm@gmail.com>
2010-11-19 03:49:40 +0300

    finished 'page_links_widget'

--- src/class.page_links_widget.ns22493.php
+++ src/class.page_links_widget.ns22493.php
@@ -18,12 +18,28 @@
 
 */
 
+
 class page_links_widget__ns22493 {
+    protected $_LABEL_TEMPLATE = '%s';
     protected $_items_limit;
     protected $_items_offset;
     protected $_items_count;
     protected $_get_link_html;
     protected $_size;
+    protected $_curr_page;
+    protected $_last_page;
+    
+    public function offset_from_page($page) {
+        $offset = ($page - 1) * $this->_items_limit;
+        
+        return $offset;
+    }
+    
+    public function page_from_offset($offset) {
+        $page = intval($offset / $this->_items_limit) + 1;
+        
+        return $page;
+    }
     
     public function __construct($items_limit, $items_offset, $items_count, $get_link_html, $size) {
         $this->_items_limit = $items_limit;
@@ -31,17 +47,97 @@ class page_links_widget__ns22493 {
         $this->_items_count = $items_count;
         $this->_get_link_html = $get_link_html;
         $this->_size = $size;
+        
+        $this->_curr_page = $this->page_from_offset($this->_items_offset);
+        $this->_last_page = intval(ceil(floatval($this->_items_count) / $this->_items_limit));
+    }
+    
+    protected function _get_link_html($render_offset, $render_page, $last_rendered_page) {
+        $html = '';
+        
+        if($last_rendered_page !== NULL && $render_page - $last_rendered_page != 1) {
+            $html .= '... ';
+        }
+        
+        if($render_page != $this->_curr_page) {
+            $html .= call_user_func(
+                $this->_get_link_html,
+                $render_offset,
+                sprintf($this->_LABEL_TEMPLATE, $render_page)
+            );
+        } else {
+            $html .= sprintf($this->_LABEL_TEMPLATE, $render_page);
+        }
+        
+        return $html;
     }
     
     public function get_widget() {
-        $html = '';
+        $was_rendered_page = array();
+        $last_rendered_page = NULL;
+        $html_elems = array();
         
-        // TEST:
-        $html .=
-            join(' ', array(
-                call_user_func($this->_get_link_html, 0, '[0]'),
-                call_user_func($this->_get_link_html, 3, '[1]'),
-            ));
+        // первые элементы:
+        
+        for(
+            $page = 1;
+            $page <= $this->_size + 1;
+            ++$page
+        ) {
+            if(
+                !in_array($page, $was_rendered_page) &&
+                $page <= $this->_last_page
+            ) {
+                $offset = $this->offset_from_page($page);
+                
+                $html_elems []= $this->_get_link_html($offset, $page, $last_rendered_page);
+                $was_rendered_page []= $page;
+                $last_rendered_page = $page;
+            }
+        }
+        
+        // текущие элементы:
+        
+        for(
+            $page = $this->_curr_page - $this->_size;
+            $page <= $this->_curr_page + $this->_size;
+            ++$page
+        ) {
+            if(
+                !in_array($page, $was_rendered_page) &&
+                $page >= 1 &&
+                $page <= $this->_last_page
+            ) {
+                $offset = $this->offset_from_page($page);
+                
+                $html_elems []= $this->_get_link_html($offset, $page, $last_rendered_page);
+                $was_rendered_page []= $page;
+                $last_rendered_page = $page;
+            }
+        }
+        
+        // последние элементы:
+        
+        for(
+            $page = $this->_last_page - $this->_size;
+            $page <= $this->_last_page;
+            ++$page
+        ) {
+            if(
+                !in_array($page, $was_rendered_page) &&
+                $page >= 1
+            ) {
+                $offset = $this->offset_from_page($page);
+                
+                $html_elems []= $this->_get_link_html($offset, $page, $last_rendered_page);
+                $was_rendered_page []= $page;
+                $last_rendered_page = $page;
+            }
+        }
+        
+        // вывод результатов:
+        
+        $html = join(' ', $html_elems);
         
         return $html;
     }
