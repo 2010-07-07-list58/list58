d78f87b912247f053f834f992899a3192f1ffb9e
Andrej A Antonov (at Tower-39) <plm.tower39@gmail.com>
2011-02-04 16:53:24 +0300

    [src/class.search_items_node.ns8184.php] tiny fix to coding standards

--- src/class.search_items_node.ns8184.php
+++ src/class.search_items_node.ns8184.php
@@ -253,7 +253,9 @@ class search_items_node__ns8184 extends node__ns21085 {
         $html =
             '<div class="GroupFrame">'.
                 '<form action="'.htmlspecialchars('?node='.urlencode($this->get_arg('node'))).'" method="post">'.
-                    '<div class="Margin5Px"><label for="_search_items_node__general_search">Введите одно или несколько ключевых слов:</label></div>'.
+                    '<div class="Margin5Px">'.
+                        '<label for="_search_items_node__general_search">Введите одно или несколько ключевых слов:</label>'.
+                    '</div>'.
                     '<div class="Margin5Px">'.
                         '<input class="Width700Px" '.
                             'type="text" '.
