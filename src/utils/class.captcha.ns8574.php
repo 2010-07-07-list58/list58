d95c721c0ad109e96e7ec30eb353c62558e9906a
User Pupnz-worldnoise <pupnz-worldnoise@fe8.hc.ru>
2012-10-18 11:14:47 +0000

    detached recaptcha-conf to special conf-file

--- src/utils/class.captcha.ns8574.php
+++ src/utils/class.captcha.ns8574.php
@@ -1,60 +1,58 @@
 <?php
 
+require_once dirname(__FILE__).'/../../var/class.recaptcha_conf.ns25302.php';
 require_once dirname(__FILE__).'/import-libs/recaptcha-php-1.11/recaptchalib.php';
 
-global
-	$captcha_public_key__ns8574,
-	$captcha_private_key__ns8574,
-	$captcha_last_error__ns8574;
+global $captcha_last_error__ns8574;
 
-$captcha_public_key__ns8574 = '6LcLHLoSAAAAABHep_EjLPM8lKxuBJozOobS9MNd';
-$captcha_private_key__ns8574 = '6LcLHLoSAAAAAMX1SDPAXnUc9T_K9hYdOPLdnuBP ';
 $captcha_last_error__ns8574 = NULL;
 
-function get_captcha_last_error__ns8574() {
+function get_captcha_last_error__ns8574 () {
     global $captcha_last_error__ns8574;
     
     return $captcha_last_error__ns8574;
 }
 
-function captcha_get_html__ns8574() {
-	global $captcha_public_key__ns8574;
+function captcha_get_html__ns8574 () {
+    $recaptcha_conf = recaptcha_conf__ns25302();
+	$public_key = $recaptcha_conf['public_key'];
 	
 	$html = '';
 	
 	$html .= 
 		'<div style="min-height: 130px; ">'.
-			recaptcha_get_html($captcha_public_key__ns8574, NULL, TRUE).
+			recaptcha_get_html($public_key, NULL, TRUE).
 		'</div>';
 	
 	return $html;
 };
 
-function translate_error__ns8574($error) {
+function translate_error__ns8574 ($error) {
 	switch($error) {
 	case 'incorrect-captcha-sol': return 'Введенна неправильная Каптча. Попробуйте ещё раз';
 	default: return $error;
 	}
 }
 
-function captcha_check_answer__ns8574($fields) {
-	global
-		$captcha_private_key__ns8574,
-		$captcha_last_error__ns8574;
+function captcha_check_answer__ns8574 ($fields) {
+    global $captcha_last_error__ns8574;
+    
+    $recaptcha_conf = recaptcha_conf__ns25302();
+    $private_key = $recaptcha_conf['private_key'];
 	
-	if(!array_key_exists('recaptcha_challenge_field', $fields) ||
+	if (!array_key_exists('recaptcha_challenge_field', $fields) ||
 			!array_key_exists('recaptcha_response_field', $fields)) {
 		$captcha_last_error__ns8574 = 'Ошибка передачи параметров';
 		
 		return FALSE;
 	}
 	
-	$resp = recaptcha_check_answer($captcha_private_key__ns8574,
+	$resp = recaptcha_check_answer ($private_key,
 		$_SERVER['REMOTE_ADDR'],
 		$fields['recaptcha_challenge_field'],
 		$fields['recaptcha_response_field']);
     
-	if($resp->is_valid) {
+	if ($resp->is_valid) {
 		$captcha_last_error__ns8574 = NULL;
 		
 		return TRUE;
---
+++ var/SAMPLE.class.recaptcha_conf.ns25302.php
@@ -0,0 +1,8 @@
+<?php
+
+function recaptcha_conf__ns25302 () {
+    return array(
+        'public_key' => '6LcLHLoSAAAAABHep_EjLPM8lKxuBJozOobS9MNd',
+        'private_key' => '6LcLHLoSAAAAAMX1SDPAXnUc9T_K9hYdOPLdnuBP',
+    );
+}
